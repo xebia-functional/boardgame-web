d84f93415fc4d6db5b1031ed2f9a36aa7f29bf34
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-10 14:00:55 +0200

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import { Link } from 'wouter';
+
+export default function Header() {
+    return (
+        <header className="flex items-center justify-between p-4 bg-primary text-white">
+            <Link href="/">
+                <h1 className="text-2xl font-bold">BGG SYSTEM</h1>
+            </Link>
+            <nav>
+                <ul className="flex space-x-4">
+                    <li>
+                        <Link href="/" className="text-white hover:text-gray-300">
+                            Players
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/" className="text-white hover:text-gray-300">
+                            Plays
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/" className="text-white hover:text-gray-300">
+                            Games
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <div>
+                <input
+                    className="bg-white text-black p-1 rounded-sm"
+                    type="text"
+                    placeholder="Search..."
+                />
+            </div>
+        </header>
+    );
+}
