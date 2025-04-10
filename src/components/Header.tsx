9904b494b1a9e2b1c4a56fcd37ff5ff017751a58
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-10 14:14:20 +0200

    Responsive Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'wouter';
+import { IconMenu, IconX } from '@tabler/icons-react';
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
-        <header className="flex items-center justify-between p-4 bg-primary text-white">
+        <header className="flex items-center justify-between p-4 bg-primary text-white relative">
             <Link href="/">
                 <h1 className="text-2xl font-bold">BGG SYSTEM</h1>
             </Link>
-            <nav>
-                <ul className="flex space-x-4">
+
+            <button
+                className="md:hidden z-20 cursor-pointer"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                aria-label="Toggle menu"
+            >
+                {isMenuOpen ? <IconX stroke={2} /> : <IconMenu stroke={2} />}
+            </button>
+
+            <nav
+                className={`
+                ${isMenuOpen ? 'block' : 'hidden'}
+                absolute top-full left-0 w-full bg-primary p-4
+                md:static md:block md:w-auto md:p-0
+            `}
+            >
+                <ul className="flex flex-col md:flex-row md:space-x-4 space-y-2 md:space-y-0">
                     <li>
                         <Link href="/players" className="text-white hover:text-gray-300">
                             Players
@@ -23,9 +42,17 @@ export default function Header() {
                             Games
                         </Link>
                     </li>
+                    <li className="block md:hidden">
+                        <input
+                            className="bg-white text-black p-1 rounded-sm w-full"
+                            type="text"
+                            placeholder="Search..."
+                        />
+                    </li>
                 </ul>
             </nav>
-            <div>
+
+            <div className="hidden md:block">
                 <input
                     className="bg-white text-black p-1 rounded-sm"
                     type="text"
