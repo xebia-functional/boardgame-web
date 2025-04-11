2856ced7d0a4e9e8ff544b9a655e900d44977856
Gacha <jlgacha75@gmail.com>
2025-04-11 09:20:09 +0200

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,6 @@
 import HotGames from '../../components/home/HotGames';
+import TrailersYT from '../../components/home/TrailersYT';
 import boardGames from '/boardGames.webp';
-import { LiteYoutubeEmbed } from 'react-lite-yt-embed';
 
 export default function Home() {
     return (
@@ -12,23 +12,7 @@ export default function Home() {
                 </h2>
             </section>
             <HotGames />
-            <section className="my-8 px-4">
-                <h3 className="text-2xl font-semibold text-center mb-8">Mira nuestros trailers</h3>
-
-                <div className="flex flex-wrap justify-center gap-8">
-                    <div className="w-full sm:w-1/4 md:w-1/4 lg:w-1/4">
-                        <LiteYoutubeEmbed id="oiQ6SgBzfqY" />
-                    </div>
-
-                    <div className="w-full sm:w-1/4 md:w-1/4 lg:w-1/4">
-                        <LiteYoutubeEmbed id="p4fB42w15Tw" />
-                    </div>
-
-                    <div className="w-full sm:w-1/4 md:w-1/4 lg:w-1/4">
-                        <LiteYoutubeEmbed id="ojkScPkdgsk" />
-                    </div>
-                </div>
-            </section>
+            <TrailersYT />
         </>
     );
 }
