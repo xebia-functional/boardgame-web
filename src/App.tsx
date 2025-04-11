e01af838d5ca67a784d89069536d560cfc22eb1c
Gacha <jlgacha75@gmail.com>
2025-04-11 11:05:29 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Switch } from 'wouter';
 import Home from './pages/home/Home';
 import NotFound from './pages/NotFound/NotFound';
 import Layout from './components/Layout';
+import Search from './pages/search/Search';
 
 function App() {
     return (
         <Layout>
             <Switch>
                 <Route path="/" component={Home} />
+                <Route path="/search" component={Search} />
                 <Route>
                     <NotFound />
                 </Route>
