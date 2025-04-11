1d3a71be14ef8a57c49ab4d3ec799bf4e78ef840
Gacha <jlgacha75@gmail.com>
2025-04-11 14:56:27 +0200

    Create Plays.tsx

---
+++ src/pages/plays/Plays.tsx
@@ -0,0 +1,8 @@
+export default function Plays() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-4">Jugadas</h1>
+            <p className="text-lg">Aquí puedes ver la lista de jugadas.</p>
+        </div>
+    );
+}
