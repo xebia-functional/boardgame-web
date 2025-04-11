bce87c621cdff1ac081220e859441181f5b2a66f
Gacha <jlgacha75@gmail.com>
2025-04-11 14:56:14 +0200

    Create Players.tsx

---
+++ src/pages/players/Players.tsx
@@ -0,0 +1,8 @@
+export default function Players() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-4">Jugadores</h1>
+            <p className="text-lg">Aquí puedes ver la lista de jugadores.</p>
+        </div>
+    );
+}
