d62da699bc614eb5a66e08c901286090d97016b8
Gacha <jlgacha75@gmail.com>
2025-04-11 09:15:33 +0200

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,8 @@
-import boardGames from '/boardGames.jpg';
+import HotGames from '../../components/home/HotGames';
+import boardGames from '/boardGames.webp';
 import { LiteYoutubeEmbed } from 'react-lite-yt-embed';
 
-const Home = () => {
+export default function Home() {
     return (
         <>
             <section className="relative w-full">
@@ -10,50 +11,7 @@ const Home = () => {
                     La mejor forma de divertirte en familia
                 </h2>
             </section>
-
-            <section className="mx-auto w-full p-4">
-                <h3 className="text-3xl font-semibold text-center mb-8">
-                    Nuestros juegos más famosos 🔥
-                </h3>
-
-                <div className="flex flex-wrap justify-center gap-6 w-full">
-                    <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 w-md">
-                        <span className="text-xl font-bold mb-4">Juego 1</span>
-                        <p className="text-gray-700">
-                            Descripción breve del juego 1. Este es un juego muy popular entre los
-                            usuarios.
-                        </p>
-                    </div>
-
-                    <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 w-md">
-                        <span className="text-xl font-bold mb-4">Juego 2</span>
-                        <p className="text-gray-700">
-                            Descripción breve del juego 2. Es uno de los más jugados este año.
-                        </p>
-                    </div>
-
-                    <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 w-md">
-                        <span className="text-xl font-bold mb-4">Juego 3</span>
-                        <p className="text-gray-700">
-                            Descripción breve del juego 3. Tiene una jugabilidad muy entretenida.
-                        </p>
-                    </div>
-
-                    <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 w-md">
-                        <span className="text-xl font-bold mb-4">Juego 4</span>
-                        <p className="text-gray-700">
-                            Descripción breve del juego 4. Es uno de los más jugados este año.
-                        </p>
-                    </div>
-
-                    <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 w-md">
-                        <span className="text-xl font-bold mb-4">Juego 5</span>
-                        <p className="text-gray-700">
-                            Descripción breve del juego 5. Tiene una jugabilidad muy entretenida.
-                        </p>
-                    </div>
-                </div>
-            </section>
+            <HotGames />
             <section className="my-8 px-4">
                 <h3 className="text-2xl font-semibold text-center mb-8">Mira nuestros trailers</h3>
 
@@ -73,6 +31,4 @@ const Home = () => {
             </section>
         </>
     );
-};
-
-export default Home;
+}
