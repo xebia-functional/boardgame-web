1abd77e20c093773b2ace1a2827f5a7573d06545
SuliAragon <j.aragonmonzon@gmail.com>
2025-04-21 12:04:20 +0200

    Fix layout and improve accessibility in NotFound component

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,7 +3,7 @@ import error404 from '/error404.png';
 
 export default function NotFound() {
     return (
-        <div className="w-screen h-screen flex items-center justify-center bg-white flex-col">
+        <div className="w-full p-20 flex items-center justify-center bg-white flex-col">
             <img
                 src={error404}
                 alt="Página no encontrada"
@@ -15,7 +15,7 @@ export default function NotFound() {
             </p>
             <Link
                 to="/"
-                className="mt-6 px-6 py-2 bg-orange-400 text-black rounded hover:bg-orange-300 transition"
+                className="mt-6 px-6 py-2 bg-orange-400 text-black rounded hover:bg-orange-300 transition cursor-pointer"
             >
                 Volver al Inicio
             </Link>
