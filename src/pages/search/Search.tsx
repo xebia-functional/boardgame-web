d58c11b8c4cfe6eddf8c883a7ef9987e20ae5b7c
Jesus Chaves <82162116+GachaDev@users.noreply.github.com>
2025-04-21 10:22:22 +0200

    Add loading state to Search component and update results rendering

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useSearchParams, Link } from 'wouter';
 
 export default function Search() {
+    const [loading, setLoading] = useState(true);
     const [results, setResults] = useState<GameBGG[]>([]);
     const [searchParams] = useSearchParams();
     const query = searchParams.get('query');
@@ -11,6 +12,7 @@ export default function Search() {
         const data = await response.json();
 
         setResults(data?.items ? data.items : []);
+        setLoading(false);
     };
 
     useEffect(() => {
@@ -21,35 +23,41 @@ export default function Search() {
 
     return (
         <div className="bg-white-secondary w-full h-screen">
-            <div className="p-4">
+            <div className="flex flex-col justify-center gap-3 w-full mt-4 p-2">
                 <h2 className="bg-white w-full text-center p-4 rounded-md font-semibold shadow-md">
                     "{query}" results...
                 </h2>
-                <div className="flex flex-col justify-center gap-3 w-full mt-4">
-                    {results.length > 0 ? (
-                        results.map((game, index) => (
-                            <div
-                                key={index}
-                                className="flex justify-between items-center bg-white p-4 rounded-md shadow-md"
-                            >
-                                <h3 className="text-xl font-semibold">
-                                    {game.name.value}{' '}
-                                    <span className="font-normal">
-                                        ({game.yearpublished?.value ?? 'No year'})
-                                    </span>
-                                </h3>
-                                <Link
-                                    href={'/games/' + game.id}
-                                    className="bg-secondary text-primary font-bold px-3 py-1 rounded-md text-center cursor-pointer hover:scale-110 transition-transform"
+                {loading ? (
+                    <div className="flex justify-center items-center h-screen">
+                        <h2 className="text-3xl font-semibold">Loading...</h2>
+                    </div>
+                ) : (
+                    <div className="flex flex-col justify-center gap-3 w-full mt-4">
+                        {results.length > 0 ? (
+                            results.map((game, index) => (
+                                <div
+                                    key={index}
+                                    className="flex justify-between items-center bg-white p-4 rounded-md shadow-md"
                                 >
-                                    Show
-                                </Link>
-                            </div>
-                        ))
-                    ) : (
-                        <p>No results found for "{query}"</p>
-                    )}
-                </div>
+                                    <h3 className="text-xl font-semibold">
+                                        {game.name.value}{' '}
+                                        <span className="font-normal">
+                                            ({game.yearpublished?.value ?? 'No year'})
+                                        </span>
+                                    </h3>
+                                    <Link
+                                        href={'/games/' + game.id}
+                                        className="bg-secondary text-primary font-bold px-3 py-1 rounded-md text-center cursor-pointer hover:scale-110 transition-transform"
+                                    >
+                                        Show
+                                    </Link>
+                                </div>
+                            ))
+                        ) : (
+                            <p>No results found for "{query}"</p>
+                        )}
+                    </div>
+                )}
             </div>
         </div>
     );
